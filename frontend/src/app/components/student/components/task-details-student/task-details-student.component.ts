48b1343ff9093f3b733bd98d9265f9d05809b70a
Denis-GM <gainutdinov.deniz@yandex.ru>
2023-05-24 13:42:59 +0500

    0.0.7

---
+++ frontend/src/app/components/student/components/task-details-student/task-details-student.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-details-student',
+  templateUrl: './task-details-student.component.html',
+  styleUrls: ['./task-details-student.component.css']
+})
+export class TaskDetailsStudentComponent {
+
+}
